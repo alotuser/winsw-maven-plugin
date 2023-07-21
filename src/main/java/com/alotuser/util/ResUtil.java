1796f96a5e0bde4172edebb6348264c1f8e11674
alotuser <alotuser@163.com>
2023-07-21 11:17:31 +0800

    2.12.1

--- src/main/java/com/alotuser/util/ResUtil.java
+++ src/main/java/com/alotuser/util/ResUtil.java
@@ -13,21 +13,20 @@ public class ResUtil {
 	/**
 	 * writeBase64File
 	 * 
-	 * @param resName resName
+	 * @param readPath  readPath
 	 * @param writePath writePath
 	 */
-	public static void writeBase64File(String resName, String writePath) {
-		ClassPathResource cpr = new ClassPathResource(resName);
-		byte[] bts = FileUtil.readBytes(cpr.getFile());
+	public static void writeBase64File(String readPath, String writePath) {
+		byte[] bts = FileUtil.readBytes(readPath);
 		String enStr = Base64Encoder.encode(bts);
-		FileUtil.writeUtf8String(enStr, writePath + cpr.getName());
+		FileUtil.writeUtf8String(enStr, writePath);
 	}
 
 	/**
 	 * writeWinFile
 	 * 
-	 * @param resName resName
-	 * @param dest dest
+	 * @param resName 	resName
+	 * @param dest 		dest
 	 */
 	public static void writeWinFile(String resName, File dest) {
 		ClassPathResource cpr = new ClassPathResource(resName);
@@ -35,5 +34,16 @@ public class ResUtil {
 		byte[] bts = Base64Decoder.decode(enStr);
 		FileUtil.writeBytes(bts, dest);
 	}
+	
+//	public static void main(String[] args) {
+//		String path="D:/winsw/2.12.0/";
+//		System.out.println("=================START=====================");
+//		writeBase64File(path+"WinSW.NET2.exe"	, path+"out/WinSW-NET2.exe.yml");
+//		writeBase64File(path+"WinSW.NET4.exe"	, path+"out/WinSW-NET4.exe.yml");
+//		writeBase64File(path+"WinSW.NET461.exe"	, path+"out/WinSW-NET461.exe.yml");
+//		writeBase64File(path+"WinSW-x64.exe"	, path+"out/WinSW-x64.exe.yml");
+//		writeBase64File(path+"WinSW-x86.exe"	, path+"out/WinSW-x86.exe.yml");
+//		System.out.println("==================END======================");
+//	}
 	
 }
